08dd72be9ca43d4ceafd6bf51e1b93426515ae06
Diego Goulart <diego.goulart@edifyeducation.com.br>
2024-02-03 13:55:00 -0300

    create customer

--- src/controllers/create-customer.controller.ts
+++ src/controllers/create-customer.controller.ts
@@ -11,6 +11,8 @@ import { z } from 'zod'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { ZodValidationPipe } from 'src/pipes/zod-validation-pipes'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { CurrentUser } from 'src/auth/current-user.decorator'
+import { UserPayload } from 'src/auth/jwt.strategy'
 
 const createCustomerBodySchema = z.object({
   name: z.string(),
@@ -28,14 +30,19 @@ export class CreateCustomerController {
   @Post()
   @HttpCode(201)
   @UsePipes(new ZodValidationPipe(createCustomerBodySchema))
-  async handle(@Body() body: CreateCustomerBodySchema) {
+  async handle(
+    @Body() body: CreateCustomerBodySchema,
+    @CurrentUser() user: UserPayload,
+  ) {
     const { name, contact, phone } = body
+    const userId = user.sub
 
     await this.prisma.customer.create({
       data: {
         name,
         contact,
         phone,
+        userId,
       },
     })
   }
